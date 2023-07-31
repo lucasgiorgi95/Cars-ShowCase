c04b41ad8b2bf536c5c2b783f4231acd7c45fb25
lucasgiorgi95 <lucas1995giorgi@gmail.com>
2023-07-31 10:36:21 -0300

    new components

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Image from 'next/image'
 import { CustomButtonProps } from '@/types'
 
-function CustomButton({title, containerStyles ,handleClick, btnType}: CustomButtonProps) {
+function CustomButton({title, containerStyles ,handleClick, btnType, textStyles, rightIcon}: CustomButtonProps) {
   return (
     <button
         disabled={false}
@@ -12,9 +12,21 @@ function CustomButton({title, containerStyles ,handleClick, btnType}: CustomButt
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
-        <span className={`flex-1`}>
+        <span className={`flex-1 ${textStyles}`}>
             {title}
         </span>
+        {
+          rightIcon && (
+            <div className='relative w-6 h-6'>
+              <Image
+                src={rightIcon}
+                alt='right icon'
+                fill
+                className='object-contain'
+              />
+            </div>
+          )
+        }
     </button>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,31 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title:string;
-    containerStyles?: string;
-    handleClick?:
-    MouseEventHandler<HTMLButtonElement>
-    btnType?:"button" | "submit"
+  title: string;
+  containerStyles?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?:string
+  rightIcon?:string
+  isDisabled?: boolean
 }
 
+export interface SearchManufacturerProp {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
 
-export interface SearchManufacturerProp{
-    manufacturer: string
-    setManufacturer:(manufacturer: string)=>void
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
